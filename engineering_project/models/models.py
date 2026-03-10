7631d25498ad9d5ee428fb6ea341c2cd618413bc
mazen41 <161659553+mazen41@users.noreply.github.com>
2026-03-10 20:48:30 +0200

    Update models.py

--- engineering_project/models/models.py
+++ engineering_project/models/models.py
@@ -160,10 +160,9 @@ class ProjectProject(models.Model):
     building_type = fields.Selection([('residential', 'سكن خاص'), ('investment', 'استثماري'), ('commercial', 'تجاري'), ('industrial', 'صناعي'), ('cooperative', 'جمعيات وتعاونيات'), ('mosque', 'مساجد'), ('hangar', 'مخازن / شبرات'), ('farm', 'مزارع')], string="نوع المبنى")
     service_type = fields.Selection([('new_construction', 'بناء جديد'), ('demolition', 'هدم'), ('modification', 'تعديل'), ('addition', 'اضافة'), ('addition_modification', 'تعديل واضافة'), ('supervision_only', 'إشراف هندسي فقط'), ('renovation', 'ترميم'), ('internal_partitions', 'قواطع داخلية'), ('shades_garden', 'مظلات / حدائق')], string="نوع الخدمة")
     
-    # --- FIXED REGION & GOVERNORATE FIELDS ---
-      # --- FIXED: Use the same Many2one fields as in Engineering Core ---
+        # NEW FIELDS
     governorate_id = fields.Many2one('kuwait.governorate', string="المحافظة")
-    region_id = fields.Many2one('kuwait.region', string="المنطقة (Region)")
+    region_id = fields.Many2one('kuwait.region', string="المنطقة")
     
     @api.onchange('governorate')
     def _onchange_governorate(self):
