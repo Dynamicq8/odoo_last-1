f2c40f97d49a90bf1532bb7792111f1c576a3724
Dynamicq8 <info@dynamicsaud.com>
2026-04-15 15:35:09 -0400

    fix: update XPath for Odoo 19 task form (oe_title removed) + remove unused http import

--- engineering_project/models/models.py
+++ engineering_project/models/models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api, _, http, Command
+from odoo import models, fields, api, _, Command
 from odoo.exceptions import UserError, ValidationError
 import datetime
 import urllib.parse
